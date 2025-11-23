a2bf7c54bfd7bec093891cfb9b3a12dcd997311d
codebetterthanyesterday <fathinabrar.a@gmail.com>
2025-11-23 17:04:16 +0700

    Restore all comments feature fix #1

--- resources/views/pages/admin/comments.blade.php
+++ resources/views/pages/admin/comments.blade.php
@@ -13,6 +13,7 @@
             </div>
             <div class="flex items-center gap-3">
                 <button 
+                    x-show="filters.show_deleted === 'true' || filters.show_deleted === 'all'"
                     @click="restoreAllComments()" 
                     class="px-4 py-2 bg-green-600 hover:bg-green-700 text-white rounded-lg transition-colors font-medium flex items-center gap-2"
                     title="Pulihkan Semua Komentar">
