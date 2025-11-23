ad44889bcb542a4bbcc7e24c141accfef299bd82
codebetterthanyesterday <fathinabrar.a@gmail.com>
2025-11-23 19:21:50 +0700

    Fix admin logout

--- resources/views/components/admin/navbar.blade.php
+++ resources/views/components/admin/navbar.blade.php
@@ -76,7 +76,7 @@
                             <p class="text-xs text-gray-500">admin@example.com</p>
                         </div>
                         <div class="py-2">
-                            <a href="#" class="flex items-center gap-3 px-4 py-2 text-red-600 hover:bg-red-50">
+                            <a href="{{route('logout')}}" class="flex items-center gap-3 px-4 py-2 text-red-600 hover:bg-red-50">
                                 <i class="ri-logout-box-line"></i>
                                 <span>Logout</span>
                             </a>
